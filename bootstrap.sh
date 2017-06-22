42b3b0cfae416f737e9695dcae9ea42b6153130f
Kai <kyan729@gmail.com>
2017-06-21 21:49:51 -0400

    adding initial assignment 1 files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
+apt-get install build-essential gcc --assume-yes
 apt-get update --fix-missing
-apt-get install vim git zlib1g-dev --assume-yes
-
-cd /home/vagrant/
-sudo wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tar.xz
-sudo tar -xvf Python-3.6.1.tar.xz
-cd Python-3.6.1
-sudo ./configure
-make
-sudo make install
+apt-get install vim git --assume-yes
+apt-get install vim git python3 --assume-yes
+apt-get install vim git python3-venv --assume-yes
 
 apt-get install bpython --assume-yes
 
