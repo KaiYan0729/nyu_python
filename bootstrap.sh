f27b17618cde45bfa3f2ab57e15d2071d2cb26a8
Kai <kyan729@gmail.com>
2017-06-22 23:15:58 -0400

    updated bootstrapping file

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,19 @@
 
 apt-get install build-essential gcc --assume-yes
 apt-get update --fix-missing
-apt-get install vim git --assume-yes
-apt-get install vim git python3 --assume-yes
-apt-get install vim git python3-venv --assume-yes
-
+apt-get install vim git zlib1g-dev --assume-yes
+apt-get install python3 --assume-yes
+apt-get install python3-venv --assume-yes
 apt-get install bpython --assume-yes
 
 
-###su - vagrant -c "cd /home/vagrant && mkdir dev && cd dev && git clone https://github.com/robmarano/nyu-python.git"
+export RUN_USER="ubuntu"
+export RUNUSER_PROG="/sbin/runuser"
+
+export COMMAND=$(cat <<-'HERE1a'
+    echo "\"echo \"in .profile\" >> $HOME/.profile\""
+    echo "\"echo \"in .bashrc\" >> $HOME/.bashrc\""
+HERE1a
+)
+
+${RUNUSER_PROG} -l ${RUN_USER} -c "${COMMAND}"
