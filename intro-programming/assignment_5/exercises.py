256e118501ba2ae4ea146d05bdc8c9108db4d2a8
Kai Yan <kyan@gmail.com>
2017-07-09 11:54:30 -0400

    Assignment5

--- intro-programming/assignment_5/exercises.py
+++ intro-programming/assignment_5/exercises.py
@@ -1,41 +1,57 @@
 #!/usr/bin/env python3
-if __name__ == '__main__':
-    main()
-	
+import sys
+'''
 def to_fahrenheit(degrees_celsius):
-	degrees_fahrenheit = 9/5* degrees_celsius + 32
-    return degrees_fahrenheit
+	degrees_fahrenheit = 9/5 * degrees_celsius + 32
+	return degrees_fahrenheit
 
 def to_celsius(degrees_fahrenheit):
-	degrees_celsius = 5/9*(degrees_fahrenheit - 32)
-    return degrees_celsius
+	degrees_celsius = 5/9 * (degrees_fahrenheit - 32)
+	return degrees_celsius
 
 
+'''
 import math
 
 def get_fall_time(height):
     # gravity isn't going to change, units in m/(s^2)
     acceleration_by_gravity = 9.8
-    time_elapsed = square_root((2 * height) / acceleration_by_gravity)
+    time_elapsed = math.sqrt((2 * height) / acceleration_by_gravity)
     return time_elapsed
 
 
 
 def isVulnerable(tower_height, tower_x, tower_y, target_x, target_y):
-    muzzle_velocity = 300
+	muzzle_velocity = 300
 
     # update this line to calculate time_in_air using get_fall_time() function
-	time_in_air = get_fall_time(tower_height)
+	time_in_air = get_fall_time(float(tower_height))
+	
+	tower_range = time_in_air * muzzle_velocity
 
-    tower_range = time_in_air * muzzle_velocity
-    
-    delta_x = distance(tower_x, target_x)  # difference between tower_x and target_x
-    delta_y = distance(tower_y, target_y)  # difference between tower_y and target_y
+	delta_x = tower_x - target_x  # difference between tower_x and target_x
+	delta_y = tower_y - target_y  # difference between tower_y and target_y
 
-    separation = square_root(delta_x**2 + delta_y**2)  # the x and y deltas form a triangle, find the hypotenuse
-    if separation < tower_range:
-        print("The target is closer than the tower range, what should we return?")
-        return None
-    else:
-        print("The target is further than the tower range, what should we return?")
-        return None
+	separation = math.sqrt(delta_x**2 + delta_y**2)  # the x and y deltas form a triangle, find the hypotenuse
+	if separation < tower_range:
+		print("The target is closer than the tower range.Your encampment is vulnerable")
+		return True
+	else:
+		print("Safe! The target is further than the tower range.")
+		return False
+
+
+def main():
+	#degrees_celsius = float(sys.argv[1])
+	#print(to_fahrenheit(degrees_celsius))
+	#degrees_fahrenheit = float(sys.argv[1])
+	#print(to_celsius(degrees_fahrenheit))
+	tower_height = float(sys.argv[1])
+	tower_x = float(sys.argv[2])
+	tower_y = float(sys.argv[3])
+	target_x = float(sys.argv[4])
+	target_y = float(sys.argv[5])
+	print(isVulnerable(tower_height, tower_x, tower_y, target_x, target_y))
+
+if __name__ == '__main__':
+    main()
--- intro-programming/assignment_5/style.py
+++ intro-programming/assignment_5/style.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python3
+
 #example_1
-if __name__ == '__main__':
-    main()
-	
-foo = long_function_name(var_one, var_two,
-    var_three, var_four)
-
-
 def long_function_name(
-    var_one, var_two, var_three,
-    var_four):
-    print(var_one)
+	var_one, var_two, var_three,
+	var_four):
+	print(var_one)
+	
+foo = long_function_name(1, 2, 3, 4)
+	
 
 #example_2
 # operators: prefix or postfix?
-income = (gross_wages +
-          taxable_interest +
-          (dividends - qualified_dividends) -
-          ira_deduction -
-          student_loan_interest)
+#income = (gross_wages + taxable_interest +(dividends - qualified_dividends) - ira_deduction - student_loan_interest)
+
 
 #example_3
 def top_level_func():
-    print("important processing going on here")
+	print("important processing going on here")
 
 def second_func():
-
-
-    print("another gravely important function")
+	print("another gravely important function")
 
 
 #example_4
 def topLevelFunc():
-    print("important processing going on here")
-    mixedCaseVariables = True
+	print("important processing going on here")
+	mixedCaseVariables = True
+
+
+def main():
+	top_level_func()
+	second_func()
+	topLevelFunc()
+
+if __name__ == '__main__':
+	main()
+
+	
